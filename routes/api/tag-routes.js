2c77cf741f9d226effa39aa9efb77a019f6a6545
kevindimayuga <k.dimayuga22@gmail.com>
2023-09-12 12:33:08 -0500

    updated comments for tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,6 +3,9 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
+// this route is a GET request that will get ALL tags BY ID in our database with an HTTP request
+// it will include all the tags, their attributes and also include the product
+// and product attributes that are associated with that tag when you submit the GET request
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
@@ -24,6 +27,9 @@ router.get('/', (req, res) => {
     });
 });
 
+// this route is a GET request that will get one tag BY ID in our database with an HTTP request
+// it will include all the tag's attributes and also include the product
+// and product attributes that are associated with that tag when you submit the GET request
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
@@ -55,6 +61,8 @@ router.get('/:id', (req, res) => {
     });
 });
 
+// this route is a POST request that will CREATE a new tag BY ID in our database with an HTTP request
+// the new tag will have a new tag_name and unique ID
 router.post('/', (req, res) => {
   // create a new tag
   Tag.create({
@@ -67,6 +75,8 @@ router.post('/', (req, res) => {
     });
 });
 
+// this route is a PUT request that will UPDATE a product by ID
+// It will have an updated unique ID
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
   Tag.update(req.body, {
@@ -89,6 +99,8 @@ router.put('/:id', (req, res) => {
     });
 });
 
+// this route is a DELETE request that will DELETE a tag BY ID in our database with an HTTP request
+// the tag will no longer be in the database once this request has been sent
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
   Tag.destroy({
